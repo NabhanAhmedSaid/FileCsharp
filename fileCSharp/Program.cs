ad69b5eb14d3da729ca9390607118e0ac0b0b4b4
NabhanAhmedSaid <100470531+NabhanAhmedSaid@users.noreply.github.com>
2023-11-20 12:34:26 +0400

    Add files via upload

---
+++ fileCSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(){
+         Console.WriteLine("Write the name of file");
+        string inputName = Console.ReadLine();
+
+        string fileName = inputName;
+
+         Console.WriteLine("Write the content of the file");
+        string input = Console.ReadLine();
+        string content = input;
+
+        File.WriteAllText(fileName,content);
+        Console.Write("Done");
+    }
+}
